d885acdc738245e90b85859d44b4b714256ff191
Alddp <yl17359197694@gmail.com>
2024-01-23 21:24:18 +0800

    12.6.6 重构check_events()

---
+++ AlineInvasion/alien_invasion.py
@@ -0,0 +1,29 @@
+import pygame
+import game_functions as gf
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    # 初始化pygame、设置和屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+
+    # 框标题
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+    # 开始游戏的主循环
+    while True:
+        # 监视键盘和鼠标事件
+        gf.check_events(ship)
+        ship.update()
+        # 刷新屏幕
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
