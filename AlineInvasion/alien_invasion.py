13168a8f8b33ba735efe6f445c81fa3ee17fcf51
Alddp <yl17359197694@gmail.com>
2024-01-23 23:49:32 +0800

    可发射子弹,还未能删除消失的子弹

--- AlineInvasion/alien_invasion.py
+++ AlineInvasion/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 import game_functions as gf
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 
 
 def run_game():
@@ -17,13 +18,16 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        bullets.update()
         # 刷新屏幕
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
