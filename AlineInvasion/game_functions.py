04efb94708b8ae614f9b27fd9da91cabbc4d733f
Alddp <yl17359197694@gmail.com>
2024-01-24 17:42:14 +0800

    重构update_bullets()

--- AlineInvasion/game_functions.py
+++ AlineInvasion/game_functions.py
@@ -61,7 +61,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """更新子弹的位置,并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
@@ -69,6 +69,26 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """响应子弹和外星人的碰撞"""
+    # 删除发生碰撞的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    # 如果外星人群被消灭,就删除现有的子弹,并创建一群外星人
+    if len(aliens) == 0:
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
+
+def update_aliens(ai_settings, aliens):
+    """
+    检查是否有外星人位于屏幕边缘,并更新整群外星人的位置
+    """
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
 
 
 def get_number_aliens_x(ai_settings, alien_width):
@@ -108,3 +128,19 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+
+def check_fleet_edges(ai_settings, aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            # 一旦有外星人到达边缘,就跳出循环
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    """将整群外星人下移,并改变他们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
--- AlineInvasion/settings.py
+++ AlineInvasion/settings.py
@@ -10,8 +10,13 @@ class Settings:
         self.ship_speed_factor = 1.4
 
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1  # 1表示向右移动,-1表示向左移动
